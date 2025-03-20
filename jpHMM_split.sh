4df2e57e3eadd708d9a7775110f45add13b4ad19
David Cowan <43557995+dacowan404@users.noreply.github.com>
2025-03-19 21:58:03 -0400

    Update jpHMM_split.sh to use default R script and alignment file for HIV

--- jpHMM_split.sh
+++ jpHMM_split.sh
@@ -16,8 +16,8 @@ for file in Files*; do
 		
 	mkdir ~/jpHMM/jpHMM_tmp/tmp"$index"
 	cd ~/jpHMM/jpHMM_tmp/tmp"$index"
-	
-	~/jpHMM/src/jpHMM -s ~/jpHMM/jpHMM_tmp/"$file"  -v HIV -a ~/jpHMM/priors/emissionPriors_HIV.txt -b ~/jpHMM/priors/transition_priors.txt -i ~/jpHMM/input/HIV-1_jpHMM_alignment_1017taxa_added_A3A4A6-A8_02_AG_Virtaln_HMM_LTRMusclealn.fas -o ./ > log.txt  &
+	# call to jpHMM can be editted as need for different priors, alignment files and/or virus
+	~/jpHMM/src/jpHMM -s ~/jpHMM/jpHMM_tmp/"$file"  -v HIV -a ~/jpHMM/priors/emissionPriors_HIV.txt -b ~/jpHMM/priors/transition_priors.txt -i ~/jpHMM/input/HIV_alignment.fas -o ./ > log.txt  &
 	index=$((index+1))
 done
 wait
@@ -28,7 +28,7 @@ echo "Finished jpHMM, starting generating images"
 date
 for ((i=0; i<$index; i++)); do
 	cd ~/jpHMM/jpHMM_tmp/tmp"$i"
-	R --no-save < ~/jpHMM/src/R_posterior_probabilities_plot_added_A3A4A6-A8_02AG.R -args jpHMM >/dev/null &
+	R --no-save < ~/jpHMM/src/R_posterior_probabilities.R -args jpHMM >/dev/null &
 done
 
 wait
@@ -44,23 +44,19 @@ mkdir posterior_probabilities
 for ((i=0; i<$index; i++)); do
 	cd ~/jpHMM/jpHMM_tmp/tmp"$i"
 	postFileCount=$(find . -type f -name "posterior_probabilities*" | wc -l)
-	#for file in ~/jpHMM/jpHMM_tmp/tmp"$i"/posterior_pro*; do
 	for ((j=1; j<=postFileCount; j++)); do
-		#mv "$file" ~/jpHMM/jpHMM_output/posterior_probabilities/posterior_probabilities_for_seq_"$fileCount".txt
 		mv ./posterior_probabilities_for_seq_"$j".txt ~/jpHMM/jpHMM_output/posterior_probabilities/posterior_probabilities_for_seq_"$fileCount".txt
 		mv ./posterior_prob_plot_for_seq_"$j".png ~/jpHMM/jpHMM_output/images/posterior_prob_plot_for_seq_"$fileCount".png
 		((fileCount++))
 	done
 	
 	mv ~/jpHMM/jpHMM_tmp/tmp"$i"/log.txt ~/jpHMM/jpHMM_output/log_"$i".txt
-	#mv ~/jpHMM/jpHMM_tmp/tmp"$i"/active_alignment_columns.txt ~/jpHMM/jpHMM_output/active_alignment_columns_"$i".txt
 	if [ "$i" -eq 0 ]; then
 		mv ~/jpHMM/jpHMM_tmp/tmp"$i"/* ~/jpHMM/jpHMM_output/
 	else
 		tail -n +8 ~/jpHMM/jpHMM_tmp/tmp"$i"/alignment_to_msa.txt >> ~/jpHMM/jpHMM_output/alignment_to_msa.txt
 		tail -n +10 ~/jpHMM/jpHMM_tmp/tmp"$i"/posterior_recombination_path.txt >> ~/jpHMM/jpHMM_output/posterior_recombination_path.txt
 		cat ~/jpHMM/jpHMM_tmp/tmp"$i"/query_to_ref_alignments.txt >> ~/jpHMM/jpHMM_output/query_to_ref_alignments.txt
-		#cat ~/jpHMM/jpHMM_tmp/tmp"$i"/raw_sequences.fas >> ~/jpHMM/jpHMM_output/raw_sequences.fas
 		tail -n +13 ~/jpHMM/jpHMM_tmp/tmp"$i"/recombination.txt >> ~/jpHMM/jpHMM_output/recombination.txt
 		tail -n +14 ~/jpHMM/jpHMM_tmp/tmp"$i"/recombination_incl_gaps.txt >> ~/jpHMM/jpHMM_output/recombination_incl_gaps.txt
 		tail -n +10 ~/jpHMM/jpHMM_tmp/tmp"$i"/recombination_without_positions.txt >> ~/jpHMM/jpHMM_output/recombination_without_positions.txt
